97916c3d236e3596f108e60b7e4acf4c3a4c2af8
Akobir <akobir.qoshshayev@gmail.com>
2021-07-01 22:38:37 +0500

    prosta-taaak

---
+++ js/main.js
@@ -0,0 +1,170 @@
+$(document).ready(function () {
+  $(".nav__hide").click(function () {
+    $(".content__nav").toggle(100);
+  });
+});
+
+$(document).ready(function () {
+  $("#amaliy__link").click(function () {
+    $(".content__nav ul li .amaliy__hide").toggle(400);
+  });
+});
+$(document).ready(function () {
+  $("#laboratoriya__link").click(function () {
+    $(".content__nav ul li .laboratoriya__hide").toggle(400);
+  });
+});
+
+$(document).ready(function () {
+  $("#maruza__link").click(function (e) {
+    $(".content__nav ul li .maruza__hide").toggle(400);
+  });
+});
+$(document).ready(function () {
+  $("#taqdimot__link").click(function (e) {
+    $(".content__nav ul li .taqdimot__hide").toggle(400);
+  });
+});
+$(document).ready(function () {
+  $("#test__link").click(function (e) {
+    $(".content__nav ul li .test__hide").toggle(400);
+  });
+});
+
+$(document).ready(function () {
+  $("#1-amaliy").click(function () {
+    $(".am__1").toggle(100);
+  });
+});
+
+$(document).ready(function () {
+  $("#2-amaliy").click(function () {
+    $(".am__2").toggle(100);
+  });
+});
+
+$(document).ready(function () {
+  $("#3-amaliy").click(function () {
+    $(".am__3").toggle(100);
+  });
+});
+
+$(document).ready(function () {
+  $("#4-amaliy").click(function () {
+    $(".am__4").toggle(100);
+  });
+});
+
+$(document).ready(function () {
+  $("#1-lab").click(function () {
+    $(".lm__1").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#2-lab").click(function () {
+    $(".lm__2").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#3-lab").click(function () {
+    $(".lm__3").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#4-lab").click(function () {
+    $(".lm__4").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#5-lab").click(function () {
+    $(".lm__5").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#6-lab").click(function () {
+    $(".lm__6").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#7-lab").click(function () {
+    $(".lm__7").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#8-lab").click(function () {
+    $(".lm__8").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#9-10-lab").click(function () {
+    $(".lm__9-10").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#11-12-lab").click(function () {
+    $(".lm__11-12").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#13-14-lab").click(function () {
+    $(".lm__13-14").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#15-16-lab").click(function () {
+    $(".lm__15-16").toggle(100);
+  });
+});
+
+// maruzalar matni
+
+$(document).ready(function () {
+  $("#maruza-1").click(function () {
+    $(".mm__1").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-2").click(function () {
+    $(".mm__2").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-3").click(function () {
+    $(".mm__3").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-4").click(function () {
+    $(".mm__4").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-5").click(function () {
+    $(".mm__5").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-6").click(function () {
+    $(".mm__6").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-7").click(function () {
+    $(".mm__7").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-8").click(function () {
+    $(".mm__8").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-9").click(function () {
+    $(".mm__9").toggle(100);
+  });
+});
+$(document).ready(function () {
+  $("#maruza-10").click(function () {
+    $(".mm__10").toggle(100);
+  });
+});
